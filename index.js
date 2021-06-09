eb1639bdc007e6cdbcffdf882f8f5f03f06b4241
xinshangshangxin <xinshangshangxin@gmail.com>
2021-06-09 14:29:03 +0800

    feat: add prettier config

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  extends: ['alloy', 'alloy/typescript', 'plugin:prettier/recommended'],
-  plugins: ['prettier'],
+  extends: ["alloy", "alloy/typescript", "plugin:prettier/recommended"],
+  plugins: ["prettier"],
   env: {
     // 你的环境变量（包含多个预定义的全局变量）
     // browser: true,
@@ -15,6 +15,22 @@ module.exports = {
   },
   rules: {
     // 自定义你的规则
-    '@typescript-eslint/no-parameter-properties': ['off'],
+    "prettier/prettier": [
+      "error",
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+        trailingComma: "all",
+        bracketSpacing: true,
+        jsxBracketSameLine: false,
+        arrowParens: "always",
+        requirePragma: false,
+        insertPragma: false,
+      },
+    ],
+    "@typescript-eslint/no-parameter-properties": ["off"],
   },
 };
